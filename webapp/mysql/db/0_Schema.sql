7125a37bdafc0597617ba2dfd345f26f84e679d3
airsh <alasho1123@yahoo.co.jp>
2024-11-26 11:37:50 +0000

    色々index

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -26,6 +26,12 @@ CREATE INDEX idx_rent_id ON isuumo.estate (rent, id);
 CREATE INDEX idx_nega_popularity_id ON isuumo.estate (nega_popularity ASC, id ASC);
 CREATE SPATIAL INDEX idx_spatial ON isuumo.estate (location);
 
+CREATE INDEX idx_estate_door_width_rent ON isuumo.estate(door_width, rent);
+CREATE INDEX idx_estate_door_height_rent ON isuumo.estate(door_height, rent);
+CREATE INDEX idx_estate_sort ON isuumo.estate(door_height, door_width, rent, nega_popularity, id);
+CREATE INDEX idx_estate_filters_sort ON isuumo.estate(door_height, door_width, rent, nega_popularity, id);
+
+CREATE INDEX idx_estate_filter_sort ON isuumo.estate(door_width, door_height, nega_popularity, id);
 
 CREATE TABLE isuumo.chair
 (
@@ -45,4 +51,9 @@ CREATE TABLE isuumo.chair
 );
 
 CREATE INDEX idx_price_id ON isuumo.chair (price, id);
+CREATE INDEX idx_chair_stock_price_id ON isuumo.chair(stock, price, id);
+
+ALTER TABLE isuumo.chair ADD INDEX chair_price_idx(price, stock);
+ALTER TABLE isuumo.chair ADD INDEX chair_height_idx(height, stock);
+ALTER TABLE isuumo.chair ADD INDEX chair_kind_idx(kind, stock);
 
